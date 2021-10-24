f80f4862a7f3edf84d1a7dfe1df838ea8e7f1820
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-10-25 01:02:34 +0900

    2-02

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pybo.apps.PyboConfig',  # 파이보 앱 등록(pybo/apps.py)
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    # Question 모델 조회시 id값 대신 제목을 표시하도록 매서드 추가
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    # Question 모델과 연결 및 삭제연동
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
