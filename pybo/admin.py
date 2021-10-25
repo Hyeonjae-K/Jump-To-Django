eec88538e6703e5063f6d9308044f23c2716331e
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-10-25 11:31:46 +0900

    2-03

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    # 검색기능 추가
+    search_field = ['subject']
+
+
+# admin 페이지에 Question 모델 등록
+admin.site.register(Question, QuestionAdmin)
