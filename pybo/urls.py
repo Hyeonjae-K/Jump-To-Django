18d8501b23103f34e49a4ce0f06535a142141376
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-10-27 21:02:17 +0900

    2-06

--- pybo/urls.py
+++ pybo/urls.py
@@ -6,5 +6,7 @@ app_name = 'pybo'
 urlpatterns = [
     # ''로 요청이 들어올 경우 views.py 파일의 index 함수 호출
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail')
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/',
+         views.answer_create, name='answer_create')
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from typing import ContextManager
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
 
 
 def index(request):
@@ -14,3 +16,12 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # POST로 전송된 데이터의 'content'를 읽어 Answer 모델에 저장
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    Answer(question=question, content=request.POST.get(
+        'content'), create_date=timezone.now()).save()
+    return redirect('pybo:detail', question_id=question.id)
