3a12cca1709775f5fbcf585d6b395a2c1660cfe4
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-10-27 16:36:14 +0900

    2-05

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
     # ''로 요청이 들어올 경우 views.py 파일의 index 함수 호출
-    path('', views.index),
-    path('<int:question_id>/', views.detail)
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail')
 ]
